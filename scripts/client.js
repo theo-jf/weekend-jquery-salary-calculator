0b2237b8ce6c7ee3da47990142b0aaddbf1ff0e6
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-26 14:39:23 -0500

    Added to and sourced css and js files

---
+++ scripts/client.js
@@ -0,0 +1,9 @@
+// TODO:
+// Array of employee objects
+// Employee array displayed on loop
+// Listener that checks for delete button presses from parent list
+// Then delete that employee from the array
+
+
+
+$(readyNow);
