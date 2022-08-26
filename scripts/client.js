13735202dbd5e23c0e904abc1d00c601fa01a6c5
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-26 16:13:37 -0500

    Added basic unstyled html elements

--- scripts/client.js
+++ scripts/client.js
@@ -1,9 +1,18 @@
 // TODO:
 // Array of employee objects
 // Employee array displayed on loop
+// Table
+// Add up employee salaries, make total turn red if > $20,000
 // Listener that checks for delete button presses from parent list
 // Then delete that employee from the array
 
+// Stretch goal is to remove a deleted employee's salary from the total
+// Wahoo! You know how to do this already! 
 
-
+let employees = [];
 $(readyNow);
+
+function readyNow() {
+
+}
+
