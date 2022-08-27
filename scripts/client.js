2e8904083a2f5d7018ed2b62b826406aa75678eb
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-26 21:27:31 -0500

    Implemented readyup function calls and employee display function

--- scripts/client.js
+++ scripts/client.js
@@ -10,9 +10,30 @@
 // Wahoo! You know how to do this already! 
 
 let employees = [];
+let totalMonthlyCost = 0;
 $(readyNow);
 
 function readyNow() {
+    displayEmployees();
+    displayCost();
 
+    $('#submit').on('click', addEmployee);
+    $('#employeeTable').on('click', '#delete', deleteEmployee);
+}
+
+function displayEmployees() {
+    $('#employeeTable').empty();
+    for (let employee of employees) {
+        $('#employeeTable').append(`
+        <tr id="employeeData">
+            <td>${employee.first}</td>
+            <td>${employee.last}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td id="employeeSalary">${employee.salary}</td>
+            <td><button id="delete">Delete</button></td>
+        </tr>
+        `);
+    }
 }
 
